edb0cf9531c86cf1081f27155f1ed187d1d3c212
estherkimyunjung <estherkimyunjung@gmail.com>
2021-06-09 18:30:33 -0500

    fix bug

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ const App = () => {
     <div className="app">
       <div className="app__left">
         <div className="app__header">
-          <h1>COVID-19 Tracker</h1>
+          <h1>COVID-19 TRACKER</h1>
           <FormControl className="app__dropdown">
             <Select
               variant="outlined"
