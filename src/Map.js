a66b25a464d12db4804a9101b244a8abd8cc870f
estherkimyunjung <estherkimyunjung@gmail.com>
2021-06-09 18:49:38 -0500

    fix bugs

--- src/Map.js
+++ src/Map.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
 import "./Map.css";
 import { showDataOnMap } from "./util";
 
 function Map({ countries, casesType, center, zoom }) {
   return (
     <div className="map">
-      <MapContainer center={center} zoom={zoom}>
+      <LeafletMap center={center} zoom={zoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {showDataOnMap(countries, casesType)}
-      </MapContainer>
+      </LeafletMap>
     </div>
   );
 }
